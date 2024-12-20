c0200b90b11c30db2bf6f07daf50cd6af6b485b5
rahilazhar12 <rahil.azhar1012@gmail.com>
2024-12-21 01:24:11 +0500

    env add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/header/Navbar";
 import Footer from "./components/footer/Footer";
 import { Outlet } from "react-router-dom";
-import Cv1 from "./components/Cv/Cv1";
 
 const App = () => {
   return (
